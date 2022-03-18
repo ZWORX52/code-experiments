5bbbe419faf9f48e79e6cb2c454d5c2267b49315
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2022-03-18 16:28:57 -0400

    IMGUI WORKING YAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY

--- multilang/origins/main.cpp
+++ multilang/origins/main.cpp
@@ -10,20 +10,80 @@
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
 static void glfw_error_callback(int error, const char* description) {
-	fprintf(stderr, "GLFW Error %d: %s", error, description);
+	fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
 int main() {
 	glfwSetErrorCallback(glfw_error_callback);
 	if (!glfwInit()) return 1;
+
 	const char* glsl_version = "#version 130";
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 
 	GLFWwindow *window = glfwCreateWindow(1280, 720, "Testing", NULL, NULL);
 
 	if (window == NULL) return 1;
+	
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	int counter = 0;
+
+	// So we can clear the screen every frame
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	// Finally done with setup (I hope)
+	// Time for some real code!
+	while (!glfwWindowShouldClose(window)) {
+		// Keyboard & Mouse events
+		glfwPollEvents();
+		// Initiate new frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Test window");
+
+		ImGui::Text("Totally useful text!");
+		if (ImGui::Button("CLICK?")) {
+			counter++;
+		}
+		ImGui::SameLine();
+		ImGui::Text("count = %i", counter);
+
+		ImGui::ColorEdit3("Clear color", (float *) &clear_color);
+
+		ImGui::Text("FPS: %f", ImGui::GetIO().Framerate);
+		ImGui::End();
+
+		// Display frame
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+	}
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
 
